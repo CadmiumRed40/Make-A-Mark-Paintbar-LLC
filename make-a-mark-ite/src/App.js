fa372f96f995d7953eb16fc63945242d92b82b85
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-07-12 23:38:31 -0400

    creating a Navigation bar component

---
+++ make-a-mark-ite/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavgationBar from './components/NavigationBar/NavgationBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavgationBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ make-a-mark-ite/src/components/NavigationBar/NavgationBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavigationBar from './NavigationBar.css'
+
+function NavgationBar() {
+  return (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>Event Calendar</li>
+            <li>Private Party</li>
+            <li>Gallery</li> 
+            <li>Contact Us</li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavgationBar
