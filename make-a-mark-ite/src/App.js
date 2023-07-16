1aee619a3320a13639b089002ba7ae273deccb69
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-07-16 16:48:13 -0400

    styling

--- make-a-mark-ite/src/App.js
+++ make-a-mark-ite/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavgationBar from './components/NavigationBar/NavgationBar';
 import BookButtons from './components/BookButtons/BookButtons';
 import AboutUs from './components/AboutUs/AboutUs';
+import InstaFeed from './components/InstaFeed/InstaFeed';
 
 function App() {
   return (
